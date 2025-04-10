5e33070f0a3ca709aa5e6041457eac519cdda8bf
Chris Short <chris.short323@gmail.com>
2025-04-10 15:33:56 -0500

    correction

--- Backend_python/project1/views.py
+++ Backend_python/project1/views.py
@@ -184,30 +184,24 @@ def validate_signup_info(request):
 
 # Final - Get the posts of the users that the original user follows
 @api_view(['POST'])
-def get_following_feed(username):
+def get_following_feed(request, username):
     # Get the user info from the username
     user = User.objects.get(username=username)
     # Get the people the user follows from the user id
-    user_follows = Follows.objects.filter(user_id=user.id)
+    follow_relationships = Follows.objects.filter(user_id=user.id)
     
-    # Get the id and username of all the people the user follows
-    follow_info = []
-    for follow in user_follows:
-        follow_id = follow.id
-        follow_username = follow.username
-        data = {'id': follow_id, 'username': follow_username}
-        follow_info.append(data)
-
-    # Get the posts of the people the user follows
     post_info = []
-    for follow in follow_info:
+    for follow in follow_relationships:
+        followed_user = User.objects.get(id=follow.following_user_id)
         # Get the posts by user id
-        posts = Posts.objects.filter(user_id=follow.id)
+        posts = Posts.objects.filter(user_id=followed_user.id)
         # Serialize the post data
         serializer = PostSerializer(posts, many=True)
         # Associate username with post(s)
-        follow_post_data = {'username': follow.username, 'Posts': serializer.data}
-        post_info.append(follow_post_data)
+        post_info.append({
+            'username': followed_user.username,
+            'posts': serializer.data
+        })
 
-    return JsonResponse(post_info)
+    return Response(post_info)
 
