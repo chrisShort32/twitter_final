2869d9e8f2f19ce5cc6a3936ec1a43cb8165c7a2
Chris Short <chris.short323@gmail.com>
2025-04-12 16:48:35 -0500

    d

--- Backend_python/project1/urls.py
+++ Backend_python/project1/urls.py
@@ -16,10 +16,5 @@ urlpatterns = [
     path('api/like_unlike/', views.like_toggle, name='like_toggle'),
     path('api/reyeet_unreyeet/', views.reyeet_toggle, name='reyeet_toggle'),
     path('api/post_yeet/', views.yeet, name='yeet'),
-    path('api/user_posts/<str:username>/', views.get_user_posts, name='get_user_posts'),
-    path('api/user_info/', views.get_user_info, name='user_info'),
-    path('api/liked_posts/', views.get_liked_posts, name='liked_posts'),
-    path('api/reyeeted_posts/', views.get_reyeeted_posts, name='reyeeted_posts'),
-    path('api/follow_unfollow/', views.follow_unfollow, name='follow_toggle'),
-    path('search_users/', views.search_users, name='search_users'),
+
 ]
